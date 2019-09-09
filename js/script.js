e486397f2322f43e11aab9c6183f11040a3470e0
Stefan Gillsbro <stefan.gillsbro@gmail.com>
2019-09-09 11:32:44 +0200

    Kontrollerat vilket användarnamn/lösenord som är korrekt

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log("Ha entrado");
+//Variabler
+const correctName = "test";         //Konstantvariabler används
+const correctPassword = "1234";     //Konstantvariabler används 
+let name, pass;
+
+//DOM
+let nameInp = document.getElementById("username");
+let passwordInp = document.getElementById("password");
+let logInBtn = document.getElementById("submit");
+
+logInBtn.onclick = function () {
+    name = nameInp.value;
+    pass = passwordInp.value;
+    
+    if (name === correctName && pass === correctPassword) {
+        console.log("Log in correcto");
+    } else {
+        console.log("ERROR");
+    }
+    
+}
